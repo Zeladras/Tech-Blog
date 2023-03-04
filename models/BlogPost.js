b9955fd42e95568e8fd4f96cffe2390e59ff7ff7
Zeldras <nbauer140@gmail.com>
2023-03-03 23:32:18 -0500

    models

---
+++ models/BlogPost.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_contents: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+  }
+);
+
+module.exports = BlogPost;
---
+++ models/index.js
@@ -0,0 +1,21 @@
+const User = require("./User")
+const BlogPost = require("./BlogPost")
+const Comment = require("./Comment")
+
+BlogPost.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+})
+
+BlogPost.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
+})
+
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+})
+
+module.exports = { User, Comment, BlogPost}
